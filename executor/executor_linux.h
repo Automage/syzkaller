30daad2f6bfbec9ee4ea80e5bdf14846dc86d60b
Pranav <pranav92000@gmail.com>
2020-08-12 15:03:50 +0400

    Update executor_linux.h

--- executor/executor_linux.h
+++ executor/executor_linux.h
@@ -10,6 +10,8 @@
 #include <sys/prctl.h>
 #include <sys/syscall.h>
 #include <unistd.h>
+#include <errno.h>
+#include <string.h>
 
 const unsigned long KCOV_TRACE_PC = 0;
 const unsigned long KCOV_TRACE_CMP = 1;
@@ -127,7 +129,7 @@ static void kmcov_enable(int fd) {
 	debug("====== Enabling kmcov...\n");
 	int ret = ioctl(fd, KMCOV_ENABLE, 0);
 	if (ret)
-		fail("kmcov enable failed: ret %d", ret);
+		fail("kmcov enable failed: ret %d, err: %s", ret, strerr(errno));
 	
 	debug("====== Enabled kmcov!\n");
 }
