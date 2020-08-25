1415924dba4a040c819c95783c597ded2f5e6329
Pranav <pranav92000@gmail.com>
2020-08-25 04:28:57 +0400

    Update proc.go

--- syz-fuzzer/proc.go
+++ syz-fuzzer/proc.go
@@ -109,7 +109,7 @@ func (proc *Proc) triageInput(item *WorkTriage) {
 	// Pranav: compute du pairs for this call
 	var inputDuCover cover.DuCover
 	total, unique := inputDuCover.ComputeDuCov(item.info.MemCover, item.info.IpCover, item.info.TypeCover)
-	log.Logf(3, "====== DU Pairs: total %v unique %v PRE-INTERSECT", total, unique)
+	log.Logf(3, "====== DU Pairs: total %v unique %v eg %v PRE-INTERSECT", total, unique, item.info.MemCover[5])
 
 	// 3141 - Possible discard of call
 	if newSignal.Empty() {
