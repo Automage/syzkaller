5fb423bd55c2e55c5f9e16f7d39690ee51e8ac2b
Pranav <pranav92000@gmail.com>
2020-09-03 15:36:30 +0400

    Update proc.go

--- syz-fuzzer/proc.go
+++ syz-fuzzer/proc.go
@@ -149,7 +149,7 @@ func (proc *Proc) triageInput(item *WorkTriage) {
 	mCovDiff := proc.fuzzer.corpusMemCoverDiff(inputMemCoverSerialized)
 
 	if mCovDiff == 0 {
-		log.Logf(3, "Khushboo : Call rejected due")
+		log.Logf(3, "Khushboo : Call rejected due to no diff : len inp: %v", len(inputMemCoverSerialized))
 	}
 
 	covMetric := 0 // 0 - added by edge, 1 - added by mem, 2 - added by both
